48e210b79f0a9fc915fb6b19eedfd8e0a024ede0
joswa <joswa1992@gmail.com>
2016-10-22 09:53:30 +0530

    MapFragment Test

---
+++ app/src/main/java/j/agriculture/Overridepublic.java
@@ -0,0 +1,7 @@
+package j.agriculture;
+
+/**
+ * Created by joswa on 10/23/2016.
+ */
+public @interface Overridepublic {
+}
---
+++ app/src/main/java/j/agriculture/Utils.java
@@ -0,0 +1,14 @@
+
+package j.agriculture;
+
+
+public class Utils {
+
+    String BaseIP = "http://192.168.1.8/Cake/";
+
+
+    String APICUSTOMERDETAILS = BaseIP + "customerdetails.php";
+    String APICATEGORY = BaseIP + "category.php";
+
+
+}
